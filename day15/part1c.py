22db68ef56b3abc5a846d73408f4369ce99632a0
ajclarkin <mail@ajclarkin.co.uk>
2022-10-10 18:11:33 +0100

    Try possible moves

--- day15/part1c.py
+++ day15/part1c.py
@@ -4,9 +4,40 @@
 from turtle import position
 from mod_intcode import Intcode
 
+
+
 x, y = 0, 0
-moves = [1, 2, 3, 4]
+dx = {1: 0, 2: 0, 3: -1, 4: 1}
+dy = {1: -1, 2: 1, 3: 0, 4: 0}
 reverse = {0:0, 1: 2, 2: 1, 3: 4, 4: 3}
+
+paths = []
+
+# Only four movement commands are understood: north (1), south (2), west (3), and east (4).
+# Responses: 0 - wall / 1 - moved 1 step in that direction / 2 - moved 1 step and found destination
+
+
+
+
+def GetPossibleDirections(prev):
+    moves = [1, 2, 3, 4]
+    reverse = {0:0, 1: 2, 2: 1, 3: 4, 4: 3}
+
+    gen = (i for i in moves if i != reverse[prev])
+    options = list(gen)     # need this listcomp aboves returns a generator
+
+    for poss in options:
+        locationtype = intcode.RunIntcode(poss)
+        if locationtype == 0:
+            options.remove(poss)
+        if locationtype == 1:
+            intcode.RunIntcode(reverse[poss])
+        else:
+            ## WHAT TO DO WHEN OXYGEN FOUND
+            pass
+
+        
+
 
 
 # Initial setup - instantiate the objects
